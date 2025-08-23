5c3252ee08232c95919e1f3cf0fce3ac2aeedf4a
Semyon Dubkov <semyondbkv@gmail.com>
2025-08-23 22:48:30 +0300

    Add template

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include "seg_tree.h"
 
-int main()
+int main(int argc, char **argv)
 {
+    int l = 0, r = 0;
+    if (!(argc == 3 && sscanf(argv[1], "%d", &l) && sscanf(argv[2], "%d", &r)))
+    {
+        printf("Usage: %s l r\n", argv[0]);
+        return 0;
+    }
     int a[] = {1, 2, 3, 4, 5};
-    seg_tree<int> tr(a, 5);
+    seg_tree<int> t1(a, 5);
+    printf("%d\n", t1.get_sum(l, r));
+
+    std::string x[] = {"ab", "cd", "ef", "dh", "er"};
+    seg_tree<std::string> t2(x, 5);
+    std::cout << t2.get_sum(l, r) << std::endl;
+
     return 0;
 }
--- seg_tree.h
+++ seg_tree.h
@@ -5,10 +5,12 @@ class seg_tree
 {
 private:
     T *t = nullptr;
+    int n = 0;
 
 public:
-    seg_tree(T a[], int n)
+    seg_tree(T *a, int n)
     {
+        this->n = n;
         t = new T[2 * n - 1];
         build(a, 0, 0, n);
     }
@@ -29,17 +31,27 @@ public:
         build(a, v + 2 * (tm - tl), tm, tr);
         t[v] = t[v + 1] + t[v + 2 * (tm - tl)];
     }
-    int get_sum(int v, int tl, int tr, int ql, int qr)
+    T get_sum(int ql, int qr)
     {
-        if (tr <= ql || qr <= tl)
-        {
-            return 0;
-        }
+        ql--;
+        return get_sum_subtree(0, 0, n, ql, qr);
+    }
+    T get_sum_subtree(int v, int tl, int tr, int ql, int qr)
+    {
         if (ql <= tl && tr <= qr)
         {
             return t[v];
         }
         int tm = (tr + tl) / 2;
-        return get_sum(v + 1, tl, tm, ql, qr) + get_sum(v + 2 * (tm - tl), tm, tr, ql, qr);
+        if (tm <= ql || qr <= tl)
+        {
+            return get_sum_subtree(v + 2 * (tm - tl), tm, tr, ql, qr);
         }
+        if (tr <= ql || qr <= tm)
+        {
+            return get_sum_subtree(v + 1, tl, tm, ql, qr);
+        }
+
+        return get_sum_subtree(v + 1, tl, tm, ql, qr) + get_sum_subtree(v + 2 * (tm - tl), tm, tr, ql, qr);
+    }
 };
